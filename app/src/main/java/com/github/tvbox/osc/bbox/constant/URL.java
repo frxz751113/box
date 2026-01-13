1e1df855955325f0440b7181d73a05f7f12a3bcb
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2026-01-13 20:34:54 +0800

    Update URL.java

--- app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
+++ app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
@@ -7,9 +7,9 @@ public class URL {
     // 默认线路
     public static final String DEFAULT_API_URL = "http://1.net4.de5.net/2.json";
     // 默认仓库线路
-    public static final String DEFAULT_STORE_API_URL = "https://gitea.com/404ynnub/resource/raw/branch/main/boxsource.json";
+    public static final String DEFAULT_STORE_API_URL = "http://1.net4.de5.net/duochang.json";";
     // 默认直播线路
-    public static final String DEFAULT_LIVE_URL = "http://1.net4.de5.net/2.json";
+    public static final String DEFAULT_LIVE_URL = "";
 
 
 }
